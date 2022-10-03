ee9fa2fa9a4875b741ed27ca174402785c5b5da7
developer-kawsar <zislam229@gmail.com>
2022-10-03 15:55:02 +0600

    tab content changed

---
+++ assets/js/back-top/back.js
@@ -0,0 +1,14 @@
+up({
+
+    bottom: '2rem',
+    right: '1rem',
+    width: '2.5rem',
+    height: '2.5rem',
+    bg: 'hsl(87, 20%, 28%)',
+    src: 'assets/cursor/chevron-up-solid.svg',
+    // distance from the top to show the back to top button
+    whenShow: 400,
+    // circular button?
+    circle: true
+
+});
