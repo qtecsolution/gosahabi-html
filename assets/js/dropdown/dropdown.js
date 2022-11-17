912db83deb1f5140ee38e31e34723b30f5872aa7
developer-kawsar <kawsar.qtec@gmail.com>
2022-11-17 19:03:33 +0600

    single trip added

---
+++ assets/js/dropdown/dropdown.js
@@ -0,0 +1,16 @@
+const  personDropdown=document.getElementById('person-dropdown');
+const personContent=document.getElementById('person-content');
+function togglePerson() {
+        personContent.classList.toggle('show');
+    } 
+    document.addEventListener('click', function(e) {
+        
+        if (!personDropdown.contains(e.target)) { 
+            
+            if(personContent.classList.contains('show')) {
+                personContent.classList.remove('show')
+    }
+        }
+        
+    
+    });
