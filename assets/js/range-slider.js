483e25428bb153fa254af87793e94d3f3ea0b503
developer-kawsar <kawsar.qtec@gmail.com>
2022-11-23 11:44:55 +0600

    added slider to single trip

--- assets/js/range-slider.js
+++ assets/js/range-slider.js
@@ -17,4 +17,25 @@ $( function() {
      $( "#max" ).val( $( "#slider-range" ).slider( "values", 1 )) ;
 
 
+    //  single trip range slider 
+    $( "#singleTripSlide" ).slider({
+      range: true,
+      min: 2000,
+      max: 15000,
+      connect: true,
+      values: [ 2000, 10900 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#lowSingletrip" ).val( ui.values[ 0 ]  );
+        $( "#maxSingletrip" ).val( ui.values[ 1]  );
+      }
+    });
+
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+   $( "#lowSingletrip" ).val( $( "#singleTripSlide" ).slider( "values", 0 )) ;
+   $( "#maxSingletrip" ).val( $( "#singleTripSlide" ).slider( "values", 1 )) ;
+
+
+
   } );
