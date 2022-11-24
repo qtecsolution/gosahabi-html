b22ee0de549e8c7e9716e383fc391d6976934249
developer-kawsar <kawsar.qtec@gmail.com>
2022-11-24 18:49:13 +0600

    added transparent header to all pages

--- assets/js/header-scroll/header-scroll.js
+++ assets/js/header-scroll/header-scroll.js
@@ -5,6 +5,7 @@ function scrollFunction() {
 if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById("header").style.backgroundColor= "#ffff";
     document.getElementById("header").style.boxShadow = "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+    document.getElementById("header").style.transition = "all .3s"
 } else {
     document.getElementById("header").style.backgroundColor = "transparent";
     document.getElementById("header").style.boxShadow = "none"
