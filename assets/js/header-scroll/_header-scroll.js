9c3d2d165b45fc3459b16a586ca06e9b0855a1cb
developer-kawsar <kawsar.qtec@gmail.com>
2022-11-24 18:06:32 +0600

    added header

--- assets/js/header-scroll/_header-scroll.js
+++ assets/js/header-scroll/_header-scroll.js
@@ -4,7 +4,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
 if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById("header").style.backgroundColor= "#ffff";
+    document.getElementById("header").style.boxShadow = "rgba(149, 157, 165, 0.2) 0px 8px 24px"
 } else {
     document.getElementById("header").style.backgroundColor = "transparent";
+    document.getElementById("header").style.boxShadow = "none"
+
 }
 }
