01e51a64336981ee29865c828bef77f5ee857cfc
developer-kawsar <kawsar.qtec@gmail.com>
2022-11-28 15:47:05 +0600

    container fixed

--- assets/js/header-scroll/header-scroll.js
+++ assets/js/header-scroll/header-scroll.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 100) {
     document.getElementById("header").style.backgroundColor= "#ffff";
     document.getElementById("header").style.boxShadow = "rgba(149, 157, 165, 0.2) 0px 8px 24px"
     document.getElementById("header").style.transition = "all .3s"
